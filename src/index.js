6dc050b3a2cf0a3bdb69ddf7baca6831bbb072b2
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2022-12-07 19:00:29 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,124 @@
+// import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const elem1 = <span>Hello</span>
+const elem2 = <span>World</span>
+
+// const element = React.createElement('div', {
+//   a: 5,
+//   children: [elem1, elem2]
+// })
+
+const data = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male"
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female"
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male"
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female"
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
+    isActive: true,
+    balance: 3951,
+    gender: "male"
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes"],
+    isActive: false,
+    balance: 1498,
+    gender: "male"
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female"
+  }
+]
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// const jsxElem = (
+//   <div>
+//     {elem1}
+//     {elem2}
+//   </div >
+//   )
+
+// console.log(jsxElem);
+
+// root.render(jsxElem)
+
+
+const friend = (
+  <div>
+    <p>Name:{data[1].url}</p>
+      <p>email:{data[1].email}</p>
+      <p>eyecolor:{data[1].eyecolor}</p>
+      <p>friends:{data[1].friends}</p>
+      <p>balance:{data[1].balance}</p>
+      <p>gender:{data[1].gender}</p>
+      <button type='button'>Add to Friends</button>
+  </div>
+);
+
+root.render(friend)
+
+//=========================================
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
